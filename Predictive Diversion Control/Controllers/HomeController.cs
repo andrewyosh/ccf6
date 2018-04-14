aca9aa5942e3ce0685cd99c8d909383c45911a5f
Ny <nynpham@gmail.com>
2018-04-13 21:03:44 -0500

    combined views

--- Predictive Diversion Control/Controllers/HomeController.cs
+++ Predictive Diversion Control/Controllers/HomeController.cs
@@ -21,19 +21,6 @@ namespace ccf6.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public async System.Threading.Tasks.Task<ActionResult> GatherDataAsync()
         {
             using (StreamReader sr = new StreamReader(Server.MapPath("~/Controllers/heroin.json")))
