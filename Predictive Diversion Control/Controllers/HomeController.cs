6f01e7380914adaee6d9160caa73bd11ed66c0de
Ny <nynpham@gmail.com>
2018-04-13 21:14:06 -0500

    ggdfvgfv

--- Predictive Diversion Control/Controllers/HomeController.cs
+++ Predictive Diversion Control/Controllers/HomeController.cs
@@ -16,8 +16,50 @@ namespace ccf6.Controllers
 {    
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async System.Threading.Tasks.Task<ActionResult> IndexAsync()
         {
+            using (StreamReader sr = new StreamReader(Server.MapPath("~/Controllers/heroin.json")))
+            {
+                var heroinOverdoseList = JsonConvert.DeserializeObject<List<OverdosesModel>>(sr.ReadToEnd());
+            }
+            using (StreamReader sr = new StreamReader(Server.MapPath("~/Controllers/prescription.json")))
+            {
+                var prescriptionOverdoseList = JsonConvert.DeserializeObject<List<OverdosesModel>>(sr.ReadToEnd());
+            }
+            var waTreatmentCenters = new List<TreatmentCenterModel>();
+
+            using (StreamReader sr = new StreamReader(Server.MapPath("~/Controllers/waTreatmentCenters.json")))
+            {
+                waTreatmentCenters = JsonConvert.DeserializeObject<List<TreatmentCenterModel>>(sr.ReadToEnd());
+            }
+
+            GoogleGeocoder geocoder = new GoogleGeocoder("AIzaSyC5T01Azye6mJJ1XNVyDvDm5QIhLSOIrWE");
+            BingMapsGeocoder bingGeocoder = new BingMapsGeocoder("AjFkeZcTp7ZICdShwZucozyyTwGD4wniT0Gn9d5z7WnoN0ytlugmJYsk7wYLshkC");
+
+            var json = new List<string>();
+
+            foreach (var x in waTreatmentCenters)
+            {
+                try
+                {
+                    IEnumerable<GoogleAddress> address = await geocoder.GeocodeAsync(x.Street + "," + x.Zipcode + " " + x.City + " " + x.State);
+                    var longitude = address.Select(a => a.Coordinates.Longitude).FirstOrDefault();
+                    var latitude = address.Select(a => a.Coordinates.Latitude).FirstOrDefault();
+                    x.latitude = latitude;
+                    x.longitude = longitude;
+                }
+
+                catch (Exception e)
+                {
+                    IEnumerable<BingAddress> address = await bingGeocoder.GeocodeAsync(x.Street + "," + x.Zipcode + " " + x.City + " " + x.State);
+                    var longitude = address.Select(a => a.Coordinates.Longitude).FirstOrDefault();
+                    var latitude = address.Select(a => a.Coordinates.Latitude).FirstOrDefault();
+                    x.latitude = latitude;
+                    x.longitude = longitude;
+                }
+
+            }
+            //ViewBag.Message = country.FormattedAddress;
             return View();
         }
 
